0c373db6dc0a10a1a354d8e721539c6b8419f04b
Florian Schuett <flo.schuett@chello.at>
2026-01-31 12:25:07 +0100

    Add basic REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,29 @@
+import { createInterface } from "node:readline";
+
 export function cleanInput(input: string): string[] {
 	return input
 		.toLowerCase()
 		.trim()
 		.split(" ")
 		.filter(word => word !== "");
+}
+
+export function startREPL() {
+	const rl = createInterface({
+		input: process.stdin,
+		output: process.stdout,
+		prompt: "> ",
+	});
+
+	rl.prompt();
+
+	rl.on("line", (input) => {
+		const cleanedInput = cleanInput(input);
+		if (cleanedInput.length === 0) {
+			rl.prompt();
+			return;
+		}
+		console.log("Your command was:", cleanedInput[0]);
+		rl.prompt();
+	});
 }
